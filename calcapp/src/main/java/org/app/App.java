15659a117c3efbcdd479aed58f087e823a484a45
unknown <VVishwakarma@INMDCD0189.ics.global>
2014-11-27 15:18:03 +0530

    calcapp project

---
+++ calcapp/src/main/java/org/app/App.java
@@ -0,0 +1,19 @@
+package org.app;
+
+import org.calc.Calculator;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+     Calculator c = new Calculator();
+      
+    }
+}
